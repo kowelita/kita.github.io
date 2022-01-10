643e4f468258ab0d7bb70d2294b2995b540ebf97
Денис Максимук <59338803+DenysDevelopment@users.noreply.github.com>
2022-01-10 16:36:17 +0200

    NewDesign (#1)

--- bg_move.js
+++ bg_move.js
@@ -1,4 +1,10 @@
-$(".background-hero").on("mousemove", function(e) {
-    $(".background-hero").css({ '--x': -e.screenX / 100 + "px", '--y': -e.screenY / 100 + "px" });
-    $(".background-hero-element").css('transform', "translate(" + e.screenX / 200 + "px, " + e.screenY / 100 + "px)");
+$('.background-hero').on('mousemove', function (e) {
+	$('.background-hero').css({
+		'--x': -e.screenX / 100 + 'px',
+		'--y': -e.screenY / 100 + 'px',
+	});
+	$('.background-hero-element').css(
+		'transform',
+		'translate(' + e.screenX / 200 + 'px, ' + e.screenY / 100 + 'px)',
+	);
 });
